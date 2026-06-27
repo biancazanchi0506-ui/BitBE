2ebbe3a7fac3a8884d7f6cebc35fd344dac741f1
Bian <biancazanchi0506@gmail.com>
2026-06-27 15:46:14 -0300

    orm.ts

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -4,3 +4,22 @@ import {SglHighLighter} from "@mikro-orm/sql-highlighter";
 export const orm = await MikroORM.init({
   entities : ['./dist/shared/db/entities'], 
   entitiesTs : ['./src/shared/db/entities'],
+  dbName : 'bitacora',
+  type : 'mysql',
+  clientUrl : 'mysql://root:root@localhost:3306/bitacora',
+  highlighter : new SglHighLighter(),
+  debug : true,
+  shemaGenerator : {
+    disableForeignKeys : true,
+    createForeignKeyConstraints : true,
+    ignoreSchema : [],
+  },
+});
+export const syncSchema = async () => {
+  const generator = orm.getSchemaGenerator()
+  /*   
+  await generator.dropSchema()
+  await generator.createSchema()
+  */
+  await generator.updateSchema()
+}
